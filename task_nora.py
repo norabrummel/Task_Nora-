00bdeff366c47c66776bc2769b64a6d6ebb3d69e
norabrummel <nora.brummel@icloud.com>
2020-12-15 22:01:10 +0100

    imported sys to pass args from docker run

--- task_nora.py
+++ task_nora.py
@@ -1,10 +1,12 @@
 from fpdf import FPDF
+import sys
+
 title = "2020-12-10 Hands-on Task for Nora \n" 
-input1 = "Input 1"
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Helvetica", size=18)
 #pdf.cell(200, 10, txt=title, ln=1, align="L")
-pdf.multi_cell(0, 10, txt=title + input1, align="L")
+#pdf.multi_cell(0, 10, txt=title + input1 + "\n" + input2, align="L")
+pdf.multi_cell(0, 10, txt=title + ("%s" % sys.argv[1]), align="L")
 pdf.output("/files/2020-12-10 Hands-on Task for Nora.pdf")
          
