e0d9a5f2972ac8dca9571bbe6510bbdbf4fc931e
norabrummel <nora.brummel@icloud.com>
2020-12-15 20:00:43 +0100

    Update task_nora.py

--- task_nora.py
+++ task_nora.py
@@ -4,6 +4,6 @@ input1 = "Input 1"
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Helvetica", size=18)
-pdf.cell(200, 10, txt=title + input1, ln=1, align="L")
+pdf.cell(200, 10, txt=title + input1, ln=2, align="L")
 pdf.output("/files/2020-12-10 Hands-on Task for Nora.pdf")
          
