7aa51847c9a26f27c7c06392debbdb6ad4e31a90
norabrummel <nora.brummel@icloud.com>
2020-12-15 20:34:16 +0100

    Update task_nora.py

--- task_nora.py
+++ task_nora.py
@@ -1,9 +1,10 @@
 from fpdf import FPDF
-title = "2020-12-10 Hands-on Task for Nora " 
+title = "2020-12-10 Hands-on Task for Nora \n" 
 input1 = "Input 1"
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Helvetica", size=18)
-pdf.cell(200, 10, txt=title + input1, ln=2, align="L")
+#pdf.cell(200, 10, txt=title, ln=1, align="L")
+pdf.multi_cell(0, 10, txt=title + input1, align="L")
 pdf.output("/files/2020-12-10 Hands-on Task for Nora.pdf")
          
