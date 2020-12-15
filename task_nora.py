3f9ac84ffdfaff84fa6ad2fefc5d730d8ea7584c
norabrummel <nora.brummel@icloud.com>
2020-12-15 22:52:03 +0100

    Update task_nora.py

--- task_nora.py
+++ task_nora.py
@@ -9,7 +9,7 @@ pdf.add_page()
 pdf.set_font("Helvetica", size=18)
 pdf.multi_cell(0, 10, txt=title, align="L") 
 for i in range(1, n):
-    pdf.multi_cell(0, 10, txt=("\n%s" % sys.argv[i]), align="L") 
+    pdf.multi_cell(0, 10, txt=("%s" % sys.argv[i]), align="L") 
 
 pdf.output("/files/2020-12-10 Hands-on Task for Nora.pdf")
          
